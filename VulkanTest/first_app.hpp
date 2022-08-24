d91b10e0ec3a053f32af487c6d0ab19a0bbdf58e
Keechang Choi <49244613+keechang-choi@users.noreply.github.com>
2022-08-25 01:24:11 +0900

    lec 10 (#5)

--- VulkanTest/first_app.hpp
+++ VulkanTest/first_app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_device.hpp"
+#include "lve_game_object.hpp"
 #include "lve_model.hpp"
 #include "lve_pipeline.hpp"
 #include "lve_swap_chain.hpp"
@@ -24,7 +25,7 @@ class FirstApp {
   void run();
 
  private:
-  void loadModels();
+  void loadGameObjects();
   void createPipelineLayout();
   void createPipeline();
   void createCommandBuffers();
@@ -32,6 +33,7 @@ class FirstApp {
   void drawFrame();
   void recreateSwapChain();
   void recordCommandBuffer(int imageIndex);
+  void renderGameObjects(VkCommandBuffer commandBuffer);
 
   LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan! ckc!"};
   LveDevice lveDevice{lveWindow};
@@ -39,6 +41,6 @@ class FirstApp {
   std::unique_ptr<LvePipeline> lvePipeline;
   VkPipelineLayout pipelineLayout;
   std::vector<VkCommandBuffer> commandBuffers;
-  std::unique_ptr<LveModel> lveModel;
+  std::vector<LveGameObject> gameObjects;
 };
 }  // namespace lve
