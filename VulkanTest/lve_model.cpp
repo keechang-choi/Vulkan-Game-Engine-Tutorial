6b9ee8d5fecec0537be0f8ea7b15a4eb421d0c81
keechang-choi <rlckd159g@gmail.com>
2022-10-05 04:37:46 +0900

    tut17

--- VulkanTest/lve_model.cpp
+++ VulkanTest/lve_model.cpp
@@ -1,8 +1,30 @@
 #include "lve_model.hpp"
 
+#include "lve_utils.hpp"
+
+// libs
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 // std
 #include <cassert>
 #include <cstring>
+#include <iostream>
+#include <string>
+
+namespace std {
+template <>
+struct hash<lve::LveModel::Vertex> {
+  size_t operator()(lve::LveModel::Vertex const& vertex) const {
+    size_t seed = 0;
+    lve::hashCombine(seed, vertex.position, vertex.color, vertex.normal,
+                     vertex.uv);
+    return seed;
+  }
+};
+}  // namespace std
 
 namespace lve {
 LveModel::LveModel(LveDevice& device, const LveModel::Builder& builder)
@@ -18,6 +40,15 @@ LveModel ::~LveModel() {
     vkFreeMemory(lveDevice.device(), indexBufferMemory, nullptr);
   }
 }
+
+std::unique_ptr<LveModel> LveModel::createModelFromFile(
+    LveDevice& device, const std::string& filepath) {
+  LveModel::Builder builder{};
+  builder.loadModel(filepath);
+  std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+  return std::make_unique<LveModel>(device, builder);
+}
+
 void LveModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
   vertexCount = static_cast<uint32_t>(vertices.size());
   assert(vertexCount >= 3 && "Vertex count must be at least 3.");
@@ -125,6 +156,69 @@ LveModel::Vertex::getAttriibuteDescriptions() {
   attributeDescription[1].format = VK_FORMAT_R32G32B32_SFLOAT;
   attributeDescription[1].offset = offsetof(Vertex, color);
   return attributeDescription;
+}
+
+void LveModel::Builder::loadModel(const std::string& filepath) {
+  tinyobj::attrib_t attrib;
+  std::vector<tinyobj::shape_t> shapes;
+  std::vector<tinyobj::material_t> materials;
+  std::string warn, err;
+
+  if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err,
+                        filepath.c_str())) {
+    throw std::runtime_error(warn + err);
+  }
+
+  vertices.clear();
+  indices.clear();
+
+  std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+  for (const auto& shape : shapes) {
+    for (const auto& index : shape.mesh.indices) {
+      Vertex vertex{};
+
+      if (index.vertex_index >= 0) {
+        vertex.position = {
+            attrib.vertices[3 * index.vertex_index + 0],
+            attrib.vertices[3 * index.vertex_index + 1],
+            attrib.vertices[3 * index.vertex_index + 2],
+        };
+
+        auto colorIndex = 3 * index.vertex_index + 2;
+        // attrib color 앞자리가 채워져 있는지 확인 필요.
+        if (colorIndex < attrib.colors.size()) {
+          vertex.color = {
+              attrib.vertices[colorIndex - 2],
+              attrib.vertices[colorIndex - 1],
+              attrib.vertices[colorIndex - 0],
+          };
+        } else {
+          vertex.color = {1.f, 1.f, 1.f};
+        }
+      }
+
+      if (index.normal_index >= 0) {
+        vertex.normal = {
+            attrib.normals[3 * index.normal_index + 0],
+            attrib.normals[3 * index.normal_index + 1],
+            attrib.normals[3 * index.normal_index + 2],
+        };
+      }
+
+      if (index.texcoord_index >= 0) {
+        vertex.uv = {
+            attrib.texcoords[2 * index.texcoord_index + 0],
+            attrib.texcoords[2 * index.texcoord_index + 1],
+        };
+      }
+
+      if (uniqueVertices.count(vertex) == 0) {
+        uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+        vertices.push_back(vertex);
+      }
+      indices.push_back(uniqueVertices[vertex]);
+    }
+  }
 }
 
 }  // namespace lve
