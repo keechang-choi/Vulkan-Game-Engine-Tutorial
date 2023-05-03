699e7158910b3b20e8a2f40223b729dbcdb25c9f
keechang-choi <rlckd159g@gmail.com>
2023-05-04 00:49:42 +0900

    Fix: magFilter nearest to linear

--- src/tut_texture.cpp
+++ src/tut_texture.cpp
@@ -40,7 +40,7 @@ TutTexture::~TutTexture() {
 void TutTexture::createTextureSampler() {
   VkSamplerCreateInfo samplerInfo{};
   samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-  samplerInfo.magFilter = VK_FILTER_NEAREST;
+  samplerInfo.magFilter = VK_FILTER_LINEAR;
   samplerInfo.minFilter = VK_FILTER_LINEAR;
 
   samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
